5fd6da50cf78a5c1ac03c5b92cd01acbd5b5eb9e
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-02-04 21:29:20 +1300

    Main functionality to get the data from the API and works with other components.

---
+++ src/components/Main.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react"
+import axios from "axios"
+
+import Header from "./Header"
+import Contents from "./Contents"
+import WeatherSearch from "./WeatherSearch"
+import Data from "./Data"
+import Context from "../Context"
+
+
+   
+const Main = () => {
+
+    const api = process.env.REACT_APP_API_KEY
+
+    const [weather, setWeather] = useState()
+    const [cityName, setCityName] = useState()
+  
+
+    const api_call = async e => {
+
+        e.preventDefault()
+
+        const location = e.target.elements.location.value
+        const API_KEY = api
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+        const request = axios.get(url)
+        const response = await request
+
+        setWeather(response.data.main)
+        setCityName(response.data.name)
+    }
+
+    weather && console.log(weather)
+
+    return (
+        <div className="main">
+            <Header />
+            <Contents>
+                <Context.Provider value={{ api_call, weather, cityName }}>
+                    <WeatherSearch />
+                    {weather && <Data />}
+                </Context.Provider>
+            </Contents>
+        </div>
+    )
+}
+
+export default Main
