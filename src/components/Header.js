96fb06e860eb2be285b6bfe3f4c0e28ca866184e
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-02-04 22:09:43 +1300

    Added css import to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
 
+import '../styles/pages/header.css'
 const Header = () => {
 
     return (
         <div className="header">
-           <h1>Forecast Finder</h1>
+           <h1>Weather Search</h1>
         </div>
     )
 }
