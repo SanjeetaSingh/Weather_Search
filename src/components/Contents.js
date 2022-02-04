afee763a01fdc4778f5ce7aabe3fee9685502d1d
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-02-04 21:30:05 +1300

    Added context file to make using states easier

---
+++ src/components/Contents.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Contents = (props) => {
+
+    return (
+        <div className="content">
+            {props.children}
+        </div>
+    )
+}
+
+export default Contents
