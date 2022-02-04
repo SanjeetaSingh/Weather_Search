03d6e4feb033d0afc77278e463a8070f612e515c
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-02-04 21:35:29 +1300

    Added time and date component to the main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,22 +6,22 @@ import Contents from "./Contents"
 import WeatherSearch from "./WeatherSearch"
 import Data from "./Data"
 import Context from "../Context"
-
+import TimeDate from "./Time_Date"
 
    
 const Main = () => {
 
     const api = process.env.REACT_APP_API_KEY
-
+    
     const [weather, setWeather] = useState()
     const [cityName, setCityName] = useState()
-  
 
     const api_call = async e => {
 
         e.preventDefault()
 
         const location = e.target.elements.location.value
+
         const API_KEY = api
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
         const request = axios.get(url)
@@ -37,6 +37,7 @@ const Main = () => {
         <div className="main">
             <Header />
             <Contents>
+                <TimeDate />
                 <Context.Provider value={{ api_call, weather, cityName }}>
                     <WeatherSearch />
                     {weather && <Data />}
