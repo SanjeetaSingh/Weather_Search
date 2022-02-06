8bef9ac4e37d9c7a943595625e3aca8a3b60c017
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-02-06 21:33:23 +1300

    Changed header type and font family.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 
     return (
         <div className="header">
-           <h1 class="display-3">Weather Search</h1>
+           <h1 class="display-2">Weather Search</h1>
         </div>
     )
 }
