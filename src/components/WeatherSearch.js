b3b6aed345c54f74d5b2c8ebe0192efeed2f8db6
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-02-06 20:19:17 +1300

    Add svg arrow to button.

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -11,9 +11,13 @@ const WeatherSearch = () => {
             <div class="input-group">
                 {/* <div class="input-append"> */}
                 <form class="form-inline" onSubmit={api_call}>
-                    <input type="text" class="col-auto my-1" name="location"></input>
+                    <input type="text" class="col-auto my-1" name="location" placeholder="Search a Location"></input>
                     <span class="input-group-btn">
-                        <button class="btn btn-default" type="btn">&rarr;</button>
+                        <button class="btn btn-default" type="btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="gray" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                            </svg>
+                        </button>
                     </span>
                 </form>
                 {/* </div> */}
