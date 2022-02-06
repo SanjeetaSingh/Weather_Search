f91e1719895f018bde9e95ff07384c77cf6d0598
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-02-06 23:10:42 +1300

    Changed the submit button to a fill arrow.

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -14,8 +14,8 @@ const WeatherSearch = () => {
                     <input type="text" class="col-auto my-1" name="location" placeholder="Search a City"></input>
                     <span class="input-group-btn">
                         <button class="btn btn-default" type="btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="gray" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+                                <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z" />
                             </svg>
                         </button>
                     </span>
